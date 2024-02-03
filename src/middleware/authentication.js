7fdd12dca089c70eec47376eb1c1613a110c05af
MBH2000 <ava201466@gmail.com>
2024-02-03 13:23:24 -0800

    implementing .env

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken'
 import User from '../User/models/user-model.js'
-
+import dotenv from 'dotenv'
+dotenv.config();
 async function verifytoken(req,res,next){
     try {
         const token = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(token,'thisismyredemptionproject')
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
         const user = await User.findOne({_id:decoded._id,token})
         if(!user){
             throw new Error()
